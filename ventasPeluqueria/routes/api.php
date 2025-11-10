b1ab7ce5a3ee94aadfa2ac2e5f30621f147906f3
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2025-11-10 06:58:07 -0300

    SecurityRoutes

---
+++ ventasPeluqueria/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');*/
+
+
+Route::get("/", function(){
+    return response()->json(['message' => 'Welcome to the app']);
+});
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+
+Route::middleware('jwt')->group(function(){
+    Route::get('/user',[AuthController::class,'getUser']);
+    //Route::put('/user',[AuthController::class,'updateUser']);
+    Route::post('/logout',[AuthController::class,'logout']);
+});
